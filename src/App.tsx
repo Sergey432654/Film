f202800e9714248a5eb5e8d7ce0393ccb45d6654
Serhii Yemets <115184910+Sergey432654@users.noreply.github.com>
2025-04-06 14:27:26 +0200

    update tail

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { MOVIES } from "./data.film"
 import { useState } from "react";
+import { twMerge } from 'tailwind-merge';
 import "tailwindcss";
 
 export default function Movies() {
   const [searchTerm, setSearchTerm] = useState(''); 
   const [selectedGenre, setSelectGenre] = useState('');
+  const [selectTeam ,setSelectTeam] = useState('');
 
   const genres = Array.from(new Set(MOVIES.map(movie => movie.genre)))
 
@@ -25,12 +27,14 @@ export default function Movies() {
         placholder = "frewfwer"
         value = {searchTerm}
         onChange = {e => setSearchTerm(e.target.value)}
-        className='px-3 py-2 rounded mb-6 text-white outline-none border border-solid border-transparent focus: border-purple-500 transition-colors'
         />
 
-        <div>
+        <div className="flex items-center gap-3 mb-6">
           {genres.map(genre => 
             <button
+              className={twMerge('px-3 py-1 rounded text-white bg-zinc-900',
+              selectedGenre == genre ? 'bg-purple-600' : 'bg-zinc-900'
+              )}
               key={genre}
               onClick ={() => setSelectGenre(selectedGenre == genre ? '' : genre)}
             >
@@ -38,8 +42,8 @@ export default function Movies() {
             </button>
             )}
         </div>
-        <ul>
-          {filtredMovies.length ? filtredMovies.map(movie => <li key={movie.id}>
+        <ul className="space-y-2 ">
+          {filtredMovies.length ? filtredMovies.map(movie => <li key={movie.id} className='fade'>
             {movie.title}({movie.genre})
             </li>) : (<li>No movie</li>)
           }
